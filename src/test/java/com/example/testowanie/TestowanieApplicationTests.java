fa9f38e1c49ff93e0b310639367474929f594817
Meffiuru <kontoall2011@gmail.com>
2022-10-06 22:14:12 +0200

    Initial commit

---
+++ src/test/java/com/example/testowanie/TestowanieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testowanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestowanieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
